c216d1dcd9fcbec75afc7edebd4b5085c696d91f
Luca Maestri <luca.maestri@tuwien.ac.at>
2019-12-04 16:32:48 +0100

    [Bugfix] Remove unused import of faulty library.
    Signed-off-by: Luca Maestri <luca.maestri@tuwien.ac.at>

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {Router} from "@angular/router";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {JwtHelperService} from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
