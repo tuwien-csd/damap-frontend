431cbe949b56d341d0587b44f808faa9e90de26b
Clara Schuster <clara@cs.it.tuwien.ac.at>
2020-08-25 15:41:57 +0200

    [Layout] Style UI to match TU CI
    [WIP] Style menu.
    Create custom color palette and theme to match TU CI.
    Add color variables to style.css.
    
    refs #62884

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -9,13 +9,26 @@ import {KeycloakService} from "keycloak-angular";
 export class LayoutComponent implements OnInit {
 
   public title:string = 'Data Management Plan';
+  public token:object;
+  public name:string;
+  public username:string;
+  public roles:string[];
 
   constructor(private auth:KeycloakService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.token = this.auth.getToken();
+    this.auth.loadUserProfile().then(p => this.name = `${p.firstName} ${p.lastName}`);
+    this.username = this.auth.getUsername();
+    this.roles = this.auth.getUserRoles(true);
+  }
 
   public logout() {
     this.auth.logout();
+  }
+
+  public widescreen(): boolean {
+    return window.innerWidth >= 1024;
   }
 
 }
