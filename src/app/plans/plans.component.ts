16f9207fd361064b21e4e3ec719b5927e509d30d
Clara Schuster <clara.schuster@tuwien.ac.at>
2020-09-03 09:34:54 +0200

    [DMP] Update DMP overview page
    - Add table displaying DMPs of user
    
    refs #62717

--- src/app/plans/plans.component.ts
+++ src/app/plans/plans.component.ts
@@ -1,16 +1,48 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Dmp} from "../model/dmp";
+import {MatTableDataSource} from "@angular/material/table";
+import {BackendService} from "../services/backend.service";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-plan',
   templateUrl: './plans.component.html',
-  styleUrls: ['./plans.component.css']
+  styleUrls: ['./plans.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class PlansComponent implements OnInit {
 
-  constructor() {
+  @Input() public userDmps: Dmp[] = [];
+  dataSource = new MatTableDataSource();
+
+  readonly tableHeaders: string[] = ['title', 'created', 'modified', 'edit','history', 'remove'];
+  expandedElement: Dmp | null;
+
+  constructor(private backendService: BackendService) {
   }
 
   ngOnInit() {
+    this.getDmps();
+    this.dataSource.data = this.userDmps;
   }
 
+  removeDmp(dmp: Dmp) {
+    // todo
+  }
+
+  private getDmps() {
+    this.backendService.getDmps()
+      .subscribe(dmps => this.userDmps = dmps);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
