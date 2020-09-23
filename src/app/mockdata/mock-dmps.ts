9bd0df4364a95495360e3b1831ea3b775b67c3b4
Clara Schuster <clara.schuster@tuwien.ac.at>
2020-09-23 14:25:39 +0200

    [DMP] Allow only one project to be set per DMP
    * Use reactive forms
    * Update DMP model and mockdata accordingly
    * Update DMP overview accordingly (plans & summary)
    
    refs #62690, refs #62717

--- src/app/mockdata/mock-dmps.ts
+++ src/app/mockdata/mock-dmps.ts
@@ -2,6 +2,6 @@ import {Dmp} from "../model/dmp";
 import {PROJECTS} from "./mock-projects";
 
 export const DMPS: Dmp[] = [
-  new Dmp(123,'Mock DMP 1', PROJECTS),
-  new Dmp(456, 'Mock DMP 2', PROJECTS),
+  new Dmp(123,'Mock DMP 1', PROJECTS[0]),
+  new Dmp(456, 'Mock DMP 2', PROJECTS[1]),
 ];
