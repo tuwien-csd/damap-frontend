04c08923bc831b5848aace18f18718278c3ff532
Clara Schuster <clara.schuster@tuwien.ac.at>
2020-09-24 14:52:06 +0200

    [DMP] Update contact and contributor selection
    * Set project leader as contact when project is selected
    * Add and remove contributors
    * Set and unset contact
    * Update mock data
    
    refs #62692, refs #62690

--- src/app/mockdata/mock-projects.ts
+++ src/app/mockdata/mock-projects.ts
@@ -1,7 +1,8 @@
 import {Project} from "../model/project";
+import {PEOPLE} from "./mock-people";
 
 export const PROJECTS: Project[] = [
-  { id: 1, title: 'Mock Project 1', description: 'This is a mock project', end: new Date(), start: new Date() },
-  { id: 2, title: 'Mock Project 2', description: 'This is a mock project', end: new Date(), start: new Date() },
-  { id: 3, title: 'Mock Project 3', description: 'This is a mock project', end: new Date(), start: new Date() },
+  { id: 1, title: 'Mock Project 1', description: 'This is a mock project', end: new Date(), start: new Date(), leader: PEOPLE[0] },
+  { id: 2, title: 'Mock Project 2', description: 'This is a mock project', end: new Date(), start: new Date(), leader: PEOPLE[1] },
+  { id: 3, title: 'Mock Project 3', description: 'This is a mock project', end: new Date(), start: new Date(), leader: null },
 ];
