32b04ddca48323cf7c9f77c36a8c7c3efe1cb451
Clara Schuster <clara.schuster@tuwien.ac.at>
2020-10-20 10:51:55 +0200

    [DMP] Limit project search to suggested projects
    * Users will only be able to search their suggested project, not the project database
    
    refs #62690

---
+++ src/app/dmp/project/project-filter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ProjectFilterPipe } from './project-filter.pipe';
+
+describe('ProjectFilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ProjectFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- src/app/dmp/project/project.component.ts
+++ src/app/dmp/project/project.component.ts
@@ -1,8 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Project} from '../../model/project';
 import {BackendService} from "../../services/backend.service";
-import {Observable, Subject} from "rxjs";
-import {debounceTime, distinctUntilChanged, switchMap} from "rxjs/operators";
 import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
@@ -15,9 +13,6 @@ export class ProjectComponent implements OnInit {
   @Input() dmpForm: FormGroup;
   @Input() projects: Project[];
 
-  projects$: Observable<Project[]>;
-  private searchTerms = new Subject<string>();
-
   projectStep: FormControl;
 
   constructor(private backendService: BackendService) {
@@ -25,27 +20,23 @@ export class ProjectComponent implements OnInit {
 
   ngOnInit(): void {
     this.getProjects();
-    this.projects$ = this.searchTerms.pipe(
-      debounceTime(300),
-      distinctUntilChanged(),
-      switchMap((term: string) =>
-        this.backendService.searchProjects(term)),
-    );
     this.projectStep = this.dmpForm.get('project') as FormControl;
     this.projectStep.valueChanges.subscribe(newVal => {
-      const contact = newVal[0].leader;
-      if(contact) {
-        this.dmpForm.get('contact').setValue(contact);
+      if(newVal){
+        const contact = newVal.leader;
+        if(contact) {
+          this.dmpForm.get('contact').setValue(contact);
+        }
       }
     });
   }
 
-  unsetProject(): void {
-    this.projectStep.reset();
+  setProject(project: Project): void {
+    this.projectStep.setValue(project);
   }
 
-  searchProjects(term: string): void {
-    this.searchTerms.next(term);
+  unsetProject(): void {
+    this.projectStep.reset();
   }
 
   private getProjects(): void {
