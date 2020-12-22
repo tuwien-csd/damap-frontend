e0ef719059e2f7923441a3a5cc3bd7d4d63bbc80
Clara Schuster <clara.schuster@tuwien.ac.at>
2020-12-22 13:32:09 +0100

    [Projects] Adapt code to BE project API
    * Date representation still needs fixing
    
    refs #65240

--- src/app/mockdata/mock-dmps.ts
+++ src/app/mockdata/mock-dmps.ts
@@ -1,7 +1,6 @@
 import {Dmp} from '../domain/dmp';
-import {PROJECTS} from './mock-projects';
 
 export const DMPS: Dmp[] = [
-  new Dmp(123,'Mock DMP 1', PROJECTS[0]),
-  new Dmp(456, 'Mock DMP 2', PROJECTS[1]),
+  new Dmp(123,'Mock DMP 1'),
+  new Dmp(456, 'Mock DMP 2'),
 ];
