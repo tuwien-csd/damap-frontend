e071389db97ed62af6db0cfba1ce711b1c1bdab6
Clara Schuster <clara.schuster@tuwien.ac.at>
2021-03-17 11:05:46 +0100

    [Contributor] Add CASRAI Roles
    refs #66598

--- src/app/domain/enum/contributor-role.enum.ts
+++ src/app/domain/enum/contributor-role.enum.ts
@@ -1,5 +1,22 @@
 export enum ContributorRole {
-  OWNER = 'OWNER',
-  EDITOR = 'EDITOR',
-  GUEST = 'GUEST'
+  DataCollector = 'Data Collector',
+  DataCurator = 'Data Curator',
+  DataManager = 'Data Manager',
+  Distributor = 'Distributor',
+  Editor = 'Editor',
+  HostingInstitution = 'HostingInstitution',
+  Producer = 'Producer',
+  ProjectLeader = 'ProjectLeader',
+  ProjectManager = 'Project Manager',
+  ProjectMember = 'Project Member',
+  RegistrationAgency = 'Registration Agency',
+  RegistrationAuthority = 'Registration Authority',
+  RelatedPerson = 'Related Person',
+  Researcher = 'Researcher',
+  ResearchGroup = 'Research Group',
+  RightsHolder = 'Rights Holder',
+  Sponsor = 'Sponsor',
+  Supervisor = 'Supervisor',
+  WorkPackageLeader = 'Work Package Leader',
+  Other = 'Other'
 }
