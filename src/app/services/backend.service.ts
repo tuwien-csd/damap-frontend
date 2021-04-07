a725da5dd145f07396a36e717d5b1c73247245c1
Clara Schuster <clara.schuster@tuwien.ac.at>
2021-04-07 17:14:30 +0200

    [BackendService] Add error handling/feedback for HTTP calls
    * Add service for feedback snackbar
    * Add error handling for http calls
    * Remove unused methods
    
    refs #68074

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, throwError} from 'rxjs';
 import {Dmp} from '../domain/dmp';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../environments/environment';
@@ -7,7 +7,8 @@ import {ProjectMember} from '../domain/project-member';
 import {Project} from '../domain/project';
 import {DmpListItem} from '../domain/dmp-list-item';
 import {Repository} from '../domain/repository';
-import {map} from 'rxjs/operators';
+import {catchError, map, retry, shareReplay} from 'rxjs/operators';
+import {FeedbackService} from './feedback.service';
 
 @Injectable({
   providedIn: 'root'
@@ -19,22 +20,23 @@ export class BackendService {
   private pdbBackendUrl = this.backendUrl + 'api/pdb'
 
   constructor(
-    private http: HttpClient) {
-  }
-
-  // TODO: remove
-  me() {
-    return this.http.get<string>(`${this.backendUrl}users/me`);
+    private http: HttpClient,
+    private feedbackService: FeedbackService) {
   }
 
   getDmps(userId: string): Observable<DmpListItem[]> {
-    return this.http.get<DmpListItem[]>(`${this.backendUrl}plans/dmp-list/${userId}`).pipe();
-    // TODO: Error handling
+    return this.http.get<DmpListItem[]>(`${this.backendUrl}plans/dmp-list/${userId}`).pipe(
+      retry(3),
+      catchError(this.handleError('Failed to load plans.'))
+    );
+
   }
 
   getDmpById(id: number): Observable<Dmp> {
-    // TODO
-    return this.http.get<Dmp>(`${this.backendUrl}plans/dmp/${id}`).pipe();
+    return this.http.get<Dmp>(`${this.backendUrl}plans/dmp/${id}`).pipe(
+      retry(3),
+      catchError(this.handleError('Failed to load plan.'))
+    );
   }
 
   createDmp(editedBy: string, dmp: Dmp): Observable<{ id: number }> {
@@ -44,7 +46,8 @@ export class BackendService {
       })
     };
     return this.http.post<{ id: number }>(`${this.backendUrl}plans/save-dmp/`, {edited_by: editedBy, dmp}, httpOptions).pipe(
-      // TODO: Error handling
+      retry(3),
+      catchError(this.handleError('Failed to save plan.'))
     );
   }
 
@@ -55,41 +58,39 @@ export class BackendService {
       })
     };
     return this.http.post(`${this.backendUrl}plans/save-dmp/`, {edited_by: editedBy, dmp}, httpOptions).pipe(
-      // TODO: Error handling
+      retry(3),
+      catchError(this.handleError('Failed to update plan.'))
     );
-  }
-
-  deleteDmp(id: number): void {
-
   }
 
   getSuggestedProjects(userId: string): Observable<Project[]> {
     return this.http.get<Project[]>(`${this.pdbBackendUrl}/suggest-projects/${userId}`).pipe(
-      // TODO: Error handling
+      retry(3),
+      catchError(this.handleError('Failed to load projects.')),
+      shareReplay(1)
     );
   }
 
   getProjectMembers(projectId: number): Observable<ProjectMember[]> {
     return this.http.get<ProjectMember[]>(`${this.pdbBackendUrl}/project-staff/${projectId}`).pipe(
-      // TODO: Error Handling here
+      retry(3),
+      catchError(this.handleError('Failed to load project members.'))
     );
-
-  }
-
-  searchPerson(term: string) {
 
   }
 
   getRepositories(): Observable<any> {
     return this.http.get<Repository[]>(this.repositoryBackendUrl).pipe(
-      // TODO: Error Handling here
+      retry(3),
+      catchError(this.handleError('Failed to load repositories.'))
     );
   }
 
   getRepositoryById(id: string): Observable<any> {
     return this.http.get<any>(`${this.repositoryBackendUrl}/${id}`).pipe(
-      map(details => ({id, changes: {info: details.repository[0]}}))
-      // TODO: Error Handling here
+      map(details => ({id, changes: {info: details.repository[0]}})),
+      retry(3),
+      catchError(this.handleError('Failed to load repository details.'))
     );
   }
 
@@ -134,5 +135,12 @@ export class BackendService {
   private getFilenameFromContentDisposition(contentDisposition: string): string {
     const start = contentDisposition.lastIndexOf('filename=');
     return contentDisposition.substring(start + 9);
+  }
+
+  private handleError(message = 'Failed to load resource.') {
+    return (error: any) => {
+      this.feedbackService.error(message);
+      return throwError(error);
+    };
   }
 }
