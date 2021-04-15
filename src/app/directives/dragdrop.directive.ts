2c361fa7804e240b7900fa75cdb8049dd00f5b4c
Clara Schuster <clara.schuster@tuwien.ac.at>
2021-04-15 11:40:15 +0200

    [Datasets] Add file upload component
    refs #64937

---
+++ src/app/directives/dragdrop.directive.ts
@@ -0,0 +1,40 @@
+import {Directive, EventEmitter, HostBinding, HostListener, Output} from '@angular/core';
+
+@Directive({
+  selector: '[dragdrop]'
+})
+export class DragdropDirective {
+
+  @HostBinding('class.fileover') fileOver: boolean;
+
+  @Output() fileDropped = new EventEmitter<any>();
+
+  constructor() {
+  }
+
+  @HostListener('dragover', ['$event'])
+  public onDragOver(event) {
+    event.preventDefault(); // Prevent file from being opened
+    event.stopPropagation();
+    this.fileOver = true;
+  }
+
+  @HostListener('dragleave', ['$event'])
+  public onDragLeave(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.fileOver = false;
+  }
+
+  @HostListener('drop', ['$event'])
+  public onDrop(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.fileOver = false;
+    const files = event.dataTransfer.files;
+    if (files.length > 0) {
+      this.fileDropped.emit(files);
+    }
+  }
+
+}
