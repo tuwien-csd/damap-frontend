6db952fc67c83a43c47936025ce5e56e632e79ec
Clara Schuster <clara.schuster@tuwien.ac.at>
2021-05-19 16:00:39 +0200

    [Refactor] Rename DmpsFailedToLoad to FailedToLoadDmps
    #68074

--- src/app/store/effects/dmp.effects.ts
+++ src/app/store/effects/dmp.effects.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Actions, Effect, ofType} from '@ngrx/effects';
-import {DmpActionTypes, DmpsFailedToLoad, DmpsLoaded, LoadDmps} from '../actions/dmp.actions';
+import {DmpActionTypes, FailedToLoadDmps, DmpsLoaded, LoadDmps} from '../actions/dmp.actions';
 import {catchError, map, mergeMap} from 'rxjs/operators';
 import {BackendService} from '../../services/backend.service';
 import {of} from 'rxjs';
@@ -14,7 +14,7 @@ export class DmpEffects {
     mergeMap(action => this.backendService.getDmps(action.payload.userId)
       .pipe(
         map(dmps => new DmpsLoaded({dmps})),
-        catchError(() => of(new DmpsFailedToLoad()))
+        catchError(() => of(new FailedToLoadDmps()))
       )
     )
   );
