7916e357edb3158d78779c641b73520c338cc029
Clara Schuster <clara.schuster@tuwien.ac.at>
2021-05-19 13:28:54 +0200

    [Store] Change Dmps loaded type to LoadingState
    refs #68074

--- src/app/store/states/dmp.state.ts
+++ src/app/store/states/dmp.state.ts
@@ -1,12 +1,13 @@
 import {createEntityAdapter, EntityAdapter, EntityState} from '@ngrx/entity';
 import {DmpListItem} from '../../domain/dmp-list-item';
+import {LoadingState} from '../../domain/enum/loading-state.enum';
 
 export interface DmpState extends EntityState<DmpListItem>{
-  loaded: boolean;
+  loaded: LoadingState;
 }
 
 export const adapter: EntityAdapter<DmpListItem> = createEntityAdapter<DmpListItem>();
 
 export const initialDmpState: DmpState = adapter.getInitialState({
-  loaded: false
+  loaded: LoadingState.NOT_LOADED
 })
