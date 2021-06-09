5a86a6e932a6b3197dfb7cda8de170ded5171ee5
Clara Schuster <clara.schuster@tuwien.ac.at>
2021-06-09 10:43:35 +0200

    [Dataset] Move setting data access type to license step
    refs #69714

--- src/app/dmp/licenses/licenses.component.ts
+++ src/app/dmp/licenses/licenses.component.ts
@@ -1,7 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {FormArray} from '@angular/forms';
+import {FormArray, FormGroup} from '@angular/forms';
 import {License} from '../../domain/license';
 import {LicenseDefinitions} from '../../widgets/license-wizard/license-wizard-list';
+import {DataAccessType} from '../../domain/enum/data-access-type.enum';
 
 @Component({
   selector: 'app-dmp-licenses',
@@ -11,9 +12,11 @@ import {LicenseDefinitions} from '../../widgets/license-wizard/license-wizard-li
 
 export class LicensesComponent implements OnInit {
 
+  @Input() dmpForm: FormGroup;
   @Input() datasets: FormArray;
 
   licenses: License[] = LicenseDefinitions;
+  accessType: any = DataAccessType;
 
   constructor() { }
 
