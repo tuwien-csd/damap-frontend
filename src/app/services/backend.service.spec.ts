34565755aec40561061118a68afee4e23441ded9
Ejmi11 <ejmishkreli@gmail.com>
2021-10-19 11:07:35 +0200

    testing

--- src/app/services/backend.service.spec.ts
+++ src/app/services/backend.service.spec.ts
@@ -3,6 +3,11 @@ import {BackendService} from './backend.service';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {FeedbackService} from './feedback.service';
 import {environment} from '../../environments/environment';
+import { Project } from '../domain/project';
+import { identifierModuleUrl } from '@angular/compiler';
+import { ProjectMember } from '../domain/project-member';
+import { title } from 'process';
+import { Dmp } from '../domain/dmp';
 
 describe('BackendService', () => {
   let service: BackendService;
@@ -40,6 +45,42 @@ describe('BackendService', () => {
   });
 
 
+/*class test2 implements Dmp{
+  readonly created?: Date;
+  readonly modified?: Date;
+  readonly id: number;
+  title?: string;
+  .
+  .
+  .
+}
+
+it('should get all suggested projects', () => {
+  service.createDmp(test2).subscribe(
+    dmps => {
+
+    }
+  );
+  const req = httpTestingController.expectOne(`${environment.backendUrl}dmp`);
+  req.flush([{}]);
+});
+*/ 
+
+  it('should retrieve dmp for user', () => {
+    service.getDmpById(0).subscribe(
+      dmps => {
+        expect(dmps).toBeTruthy('No dmps returned');
+
+        const dmp = dmps.personalDataAccess;
+        expect(dmp.length).toBe(1, 'Random Dmp');
+      }
+    );
+
+    const req = httpTestingController.expectOne(`${environment.backendUrl}dmps/${0}`);
+    req.flush([{id: 1, project:{title: 'Random Dmp'}}]);
+  });
+
+
   it('should retrieve all repositories', () => {
     service.getRepositories().subscribe(
       repos => {
@@ -54,6 +95,53 @@ describe('BackendService', () => {
     const req = httpTestingController.expectOne(`${environment.backendUrl}repositories`);
     req.flush([{id: 'r3d100012810', name: 'Random Repo'}]);
   });
+
+  it('should retrieve repository by Id', () => {
+    service.getRepositoryById('r3d100012810').subscribe(
+      repos => {
+        expect(repos).toBeTruthy('No repos returned');
+        expect(repos.length).toBe(1, 'Incorrect number of repos');
+
+        const repo = repos.find(item => item.id === 'r3d100012810');
+        expect(repo.name).toBe('Random Repo');
+      }
+    );
+
+    const req = httpTestingController.expectOne(`${environment.backendUrl}repositories/${0}`);
+    req.flush([{id: 'r3d100012810', name: 'Random Repo'}]);
+  });
+
+  it('should get all suggested projects', () => {
+    service.getSuggestedProjects().subscribe(
+      (projects: Project[]) => {
+        expect(projects).toBeTruthy('No projects returned');
+        expect(projects.length).toBe(1, 'Incorrect number of projects');
+
+        const project = projects[0];
+        expect(project.title).toBe('Mock project');
+      }
+    );
+
+    const req = httpTestingController.expectOne(`${environment.backendUrl}projects`);
+    req.flush([{id:null, universityId: 1234, title:'Mock project'}]);
+  });
+
+
+  it('should get all project members', () => {
+    service.getProjectMembers(1234).subscribe(
+      (projectsMember: ProjectMember[]) => {
+        expect(projectsMember).toBeTruthy('No project members returned');
+        expect(projectsMember.length).toBe(1, 'Incorrect number of project members');
+
+        const project = projectsMember[0];
+        expect(project.roleInProject).toBe('Random member');
+      }
+    );
+
+    const req = httpTestingController.expectOne(`${environment.backendUrl}projects/${0}/staff`);
+    req.flush([{person: [{id:null, universityId: 1234, title:'Mock project'}]},{roleInProject:'Random member'}]);
+  });
+
 
   it('should search repositories by filters', () => {
     service.searchRepository({subjects: ['Cars']}).subscribe(
