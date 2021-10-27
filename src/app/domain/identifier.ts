b0cacd81bb2a63fad31f45a7373240ce9688b1d3
Clara Schuster <clara.schuster@tuwien.ac.at>
2021-10-27 10:14:32 +0200

    Add affiliation to person, refs #72835

--- src/app/domain/identifier.ts
+++ src/app/domain/identifier.ts
@@ -1,4 +1,8 @@
+import {IdentifierType} from './enum/person-id-type.enum';
+
 export interface Identifier {
+
   readonly identifier: string;
-  readonly type: string;
+  readonly type: IdentifierType;
+
 }
