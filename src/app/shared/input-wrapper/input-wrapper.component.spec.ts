32485da2accd640fab7834f14d9e65c6a62bb451
Clara Schuster <clara.schuster@tuwien.ac.at>
2021-11-10 15:07:07 +0100

    Fix tests for input/textarea wrapper, refs #70295

--- src/app/shared/input-wrapper/input-wrapper.component.spec.ts
+++ src/app/shared/input-wrapper/input-wrapper.component.spec.ts
@@ -1,6 +1,10 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {InputWrapperComponent} from './input-wrapper.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('InputFormFieldWrapperComponent', () => {
   let component: InputWrapperComponent;
@@ -8,7 +12,8 @@ describe('InputFormFieldWrapperComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InputWrapperComponent ]
+      imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule, NoopAnimationsModule],
+      declarations: [InputWrapperComponent]
     })
     .compileComponents();
   });
@@ -16,6 +21,8 @@ describe('InputFormFieldWrapperComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(InputWrapperComponent);
     component = fixture.componentInstance;
+    component.label = 'Label';
+    component.control = new FormControl();
     fixture.detectChanges();
   });
 
