989230085f87272dac0b161712b3e0e242790452
Clara Schuster <clara.schuster@tuwien.ac.at>
2021-11-10 15:07:07 +0100

    Fix tests for input/textarea wrapper, refs #70295

--- src/app/shared/textarea-wrapper/textarea-wrapper.component.spec.ts
+++ src/app/shared/textarea-wrapper/textarea-wrapper.component.spec.ts
@@ -1,6 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TextareaWrapperComponent } from './textarea-wrapper.component';
+import {TextareaWrapperComponent} from './textarea-wrapper.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('TextareaWrapperComponent', () => {
   let component: TextareaWrapperComponent;
@@ -8,7 +12,8 @@ describe('TextareaWrapperComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TextareaWrapperComponent ]
+      imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule, NoopAnimationsModule],
+      declarations: [TextareaWrapperComponent]
     })
     .compileComponents();
   });
@@ -16,6 +21,8 @@ describe('TextareaWrapperComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TextareaWrapperComponent);
     component = fixture.componentInstance;
+    component.label = 'Label';
+    component.control = new FormControl();
     fixture.detectChanges();
   });
 
