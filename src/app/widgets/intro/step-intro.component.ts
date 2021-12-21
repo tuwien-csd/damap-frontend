8096de0fb9d67604d324abfc860258150ab05297
Clara Schuster <clara.schuster@tuwien.ac.at>
2021-12-21 14:35:22 +0100

    Add step introductions
    Update testfiles accordingly
    
    refs #71629

---
+++ src/app/widgets/intro/step-intro.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-step-intro',
+  templateUrl: './step-intro.component.html',
+  styleUrls: ['./step-intro.component.css']
+})
+export class StepIntroComponent {
+
+  constructor() { }
+
+}
