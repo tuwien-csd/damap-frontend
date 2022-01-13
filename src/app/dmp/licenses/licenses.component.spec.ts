6267e2e334bcf1b0fd0e642fd5c7ec302a9d89b6
Clara Schuster <clara.schuster@tuwien.ac.at>
2022-01-13 23:18:35 +0100

    Add EN translation for licenses component
    refs #73683

--- src/app/dmp/licenses/licenses.component.spec.ts
+++ src/app/dmp/licenses/licenses.component.spec.ts
@@ -1,17 +1,19 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { LicensesComponent } from './licenses.component';
+import {LicensesComponent} from './licenses.component';
+import {TranslateTestingModule} from '../../testing/translate-testing/translate-testing.module';
+import {MatCardModule} from '@angular/material/card';
 
 describe('LicensesComponent', () => {
   let component: LicensesComponent;
   let fixture: ComponentFixture<LicensesComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LicensesComponent ]
-    })
-    .compileComponents();
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MatCardModule, TranslateTestingModule],
+      declarations: [LicensesComponent]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LicensesComponent);
