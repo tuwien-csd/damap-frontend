cbcce916d5ae5f2df098f7de066b9e72f0af79dc
Clara Schuster <clara.schuster@tuwien.ac.at>
2022-02-03 14:24:07 +0100

    Add links to landing page text
    refs #73907

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,10 +2,18 @@ import {NgModule} from '@angular/core';
 import {DashboardComponent} from './dashboard.component';
 import {CommonModule} from '@angular/common';
 import {TranslateModule} from '@ngx-translate/core';
+import {MatButtonModule} from '@angular/material/button';
+import {RouterModule} from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, TranslateModule],
+  imports: [
+    CommonModule, RouterModule, TranslateModule,
+    // Angular Materials
+    MatButtonModule, MatIconModule
+  ],
   exports: [CommonModule, DashboardComponent]
 })
-export class DashboardModule {}
+export class DashboardModule {
+}
