49fbe81817ed607878acf4c4b852295cc7e8aa2d
Clara Schuster <clara.schuster@tuwien.ac.at>
2022-02-03 14:24:07 +0100

    Add links to landing page text
    refs #73907

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -2,6 +2,8 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {DashboardComponent} from './dashboard.component';
 import {TranslateTestingModule} from '../testing/translate-testing/translate-testing.module';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -10,7 +12,7 @@ describe('DashboardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DashboardComponent],
-      imports: [TranslateTestingModule]
+      imports: [MatButtonModule, MatIconModule, TranslateTestingModule]
     })
       .compileComponents();
   });
