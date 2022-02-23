4e30ca543d1105691bdd959ca878cb2ac933d8ca
Clara Schuster <clara.schuster@tuwien.ac.at>
2022-02-23 11:01:07 +0100

    Add data quality questions
    refs #71633

--- src/app/dmp/doc-data-quality/doc-data-quality.component.ts
+++ src/app/dmp/doc-data-quality/doc-data-quality.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
+import {DataQualityType} from '../../domain/enum/data-quality-type.enum';
 
 @Component({
   selector: 'app-dmp-doc-data-quality',
@@ -13,20 +14,32 @@ export class DocDataQualityComponent {
   optionsDataGeneration: string[] = ['Specific software will be used to process the raw data', 'Specific software will be used to conduct statistics', 'Specific software will be used to create graphical representations', 'Specific software will be used to make drawings'];
   optionsStructureAndVersioning: string[] = ['The filenames will follow the projects naming convention as defined in [add document name] and include a timestamp of creation. Version control is automated.', 'The respective work package leader will handle the structure and versioning of the research data.'];
 
+  readonly translateDataQualityPrefixEnum = 'enum.dataquality.'
+  readonly dataQualityOptions: any = DataQualityType;
+
+  originalOrder = (): number => 0;
 
   constructor() {
   }
 
   get metadata(): FormControl {
-    return this.docDataStep?.get('metadata') as FormControl;
+    return this.docDataStep.get('metadata') as FormControl;
   }
 
   get dataGeneration(): FormControl {
-    return this.docDataStep?.get('dataGeneration') as FormControl;
+    return this.docDataStep.get('dataGeneration') as FormControl;
   }
 
   get structure(): FormControl {
-    return this.docDataStep?.get('structure') as FormControl;
+    return this.docDataStep.get('structure') as FormControl;
+  }
+
+  get isOtherDataQualitySelected() {
+    return this.docDataStep.controls.dataQuality.value?.includes(DataQualityType.OTHERS);
+  }
+
+  get otherDataQuality(): FormControl {
+    return this.docDataStep.get('otherDataQuality') as FormControl;
   }
 
 }
