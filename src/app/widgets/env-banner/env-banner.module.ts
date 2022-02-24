4f0c1c368ac14dcfb62545f900fa66f0852e2937
Clara Schuster <clara.schuster@tuwien.ac.at>
2022-02-24 16:35:16 +0100

    Add dev/test environment banner
    refs #76201

---
+++ src/app/widgets/env-banner/env-banner.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+import {EnvBannerComponent} from './env-banner.component';
+import {CommonModule} from '@angular/common';
+import {TranslateModule} from '@ngx-translate/core';
+import {MatCardModule} from '@angular/material/card';
+
+@NgModule({
+  declarations: [EnvBannerComponent],
+  imports: [
+    CommonModule,
+    TranslateModule,
+
+    // Materials
+    MatCardModule
+  ],
+  exports: [
+    CommonModule,
+    EnvBannerComponent,
+
+    // Materials
+    MatCardModule
+  ]
+})
+export class EnvBannerModule {}
