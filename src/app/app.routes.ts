882a4ef36c48c69bd1d0164b31b0dae7a8b0405d
Zeno Casellato <zeno.casellato@tuwien.ac.at>
2022-04-07 19:35:50 +0200

    Revert "Lazy load DmpModule & use separate TranslationModule"
    This reverts commit 4d4733c0e2a8bd38a9ae0bba0566d736c01096ee.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import {Routes} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {PlansComponent} from './plans/plans.component';
+import {DmpComponent} from './dmp/dmp.component';
 import {LayoutComponent} from './layout/layout.component';
 import {AuthGuard} from './guard/auth.guard';
 import {ConsentGuard} from './guard/consent.guard';
+import {VersionListComponent} from './version/version-list/version-list.component';
+import {VersionViewComponent} from './version/version-view/version-view.component';
 
 export const APP_ROUTES: Routes = [
   {
@@ -11,9 +14,9 @@ export const APP_ROUTES: Routes = [
       {path: '', component: DashboardComponent},
       {path: 'dashboard', component: DashboardComponent},
       {path: 'plans', component: PlansComponent},
-      {
-        path: 'dmp',
-        loadChildren: () => import('./dmp/dmp.module').then(m => m.DmpModule)
-      },
+      {path: 'dmp', component: DmpComponent},
+      {path: 'dmp/:id', component: DmpComponent},
+      {path: 'dmp/:id/version', component: VersionListComponent},
+      {path: 'dmp/:id/version/:revision', component: VersionViewComponent}
     ]
   }];
