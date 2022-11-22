f5f174378c09f0908ab7ac5026d08d58a2a7fa4e
Laura TH <112856634+lpandath@users.noreply.github.com>
2022-11-22 11:02:31 +0100

    Enhance plans overview (#55)
    * enhance plan overview
    * minor change on styling

--- libs/damap/src/lib/widgets/dmp-table/dmp-table.module.ts
+++ libs/damap/src/lib/widgets/dmp-table/dmp-table.module.ts
@@ -5,10 +5,12 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
@@ -26,7 +28,9 @@ import { TranslateModule } from '@ngx-translate/core';
     MatDividerModule,
     MatPaginatorModule,
     MatSelectModule,
-    MatSortModule
+    MatSortModule,
+    MatMenuModule,
+    MatTooltipModule
   ],
   declarations: [DmpTableComponent],
   exports: [
@@ -41,7 +45,9 @@ import { TranslateModule } from '@ngx-translate/core';
     MatDividerModule,
     MatPaginatorModule,
     MatSelectModule,
-    MatSortModule
+    MatSortModule,
+    MatMenuModule,
+    MatTooltipModule
   ]
 })
 
