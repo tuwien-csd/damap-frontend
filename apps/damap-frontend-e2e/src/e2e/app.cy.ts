8b366cb74e55debd94d790f482a27422ee8a7039
Clara Schuster <98585998+clarascu@users.noreply.github.com>
2022-11-29 14:01:30 +0100

    Add plans overview test

--- apps/damap-frontend-e2e/src/e2e/app.cy.ts
+++ apps/damap-frontend-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,50 @@
-import { getGreeting } from '../support/app.po';
+describe("damap-frontend", () => {
+  beforeEach(() => cy.visit("/"));
 
-describe('damap-frontend', () => {
-  beforeEach(() => cy.visit('/'));
+  it("should load plans", () => {
+    cy.fixture("users").then((users) => {
+      cy.login(users.username, users.password);
+      // Go to plans
+      cy.get('div.button-row-left > button').first().click();
+      // Check loading bar
+      cy.get('div.mat-progress-bar-element').should('exist');
+      // Check table
+      cy.get('table').should('exist');
+      cy.logout();
+    });
+  });
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  // TODO
+  it("should create and autosave dmp", () => {
+    cy.fixture("users").then((users) => {
+      cy.login(users.username, users.password);
+      // Create new dmp
+      // Create new project
+      // Add new data
+      // Use file upload
+      // Check autosave
+      // Search for repositories
+      // Go back to plans
+      // Check if dmp shows in list
+      cy.logout();
+    });
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome damap-frontend');
+  // TODO
+  it("should update and autosave dmp", () => {
+    cy.fixture("users").then((users) => {
+      cy.login(users.username, users.password);
+      // Create new dmp
+      // Select project from db
+      // Search and add person
+      // Add reused data
+      // Add storage
+      // Check autosave
+      // Set retention period
+      // Reuse of data
+      // Add costs
+      // Check if dmp shows in list
+      cy.logout();
+    });
   });
 });
