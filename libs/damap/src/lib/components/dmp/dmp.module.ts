f1c92b1ebb2b4c087bd7bf6bb8ff905b9c4bdf05
Laura TH <112856634+lpandath@users.noreply.github.com>
2022-12-12 11:01:28 +0100

    Autosave scroll to the top of the new section (#37)

--- libs/damap/src/lib/components/dmp/dmp.module.ts
+++ libs/damap/src/lib/components/dmp/dmp.module.ts
@@ -1,27 +1,28 @@
-import {NgModule} from '@angular/core';
-import {DmpComponent} from './dmp.component';
-import {CommonModule} from '@angular/common';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {MatStepperModule} from '@angular/material/stepper';
-import {ReuseModule} from './reuse/reuse.module';
-import {SummaryModule} from './summary/summary.module';
+
+import {CommonModule} from '@angular/common';
 import {CostsModule} from './costs/costs.module';
+import {DMP_ROUTES} from './dmp.routes';
 import {DataDeletionModule} from './data-deletion/data-deletion.module';
 import {DataStorageModule} from './data-storage/data-storage.module';
+import {DmpActionsModule} from './dmp-actions/dmp-actions.module';
+import {DmpComponent} from './dmp.component';
 import {DocDataQualityModule} from './doc-data-quality/doc-data-quality.module';
+import {FormsModule} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 import {LegalEthicalAspectsModule} from './legal-ethical-aspects/legal-ethical-aspects.module';
 import {LicensesModule} from './licenses/licenses.module';
+import { MatStepperModule } from '@angular/material/stepper';
+import {MultiTranslateHttpLoader} from 'ngx-translate-multi-http-loader';
+import {NgModule} from '@angular/core';
 import {PeopleModule} from './people/people.module';
 import {ProjectModule} from './project/project.module';
 import {RepoModule} from './repo/repo.module';
-import {SpecifyDataModule} from './specify-data/specify-data.module';
-import {HttpClient} from '@angular/common/http';
-import {MultiTranslateHttpLoader} from 'ngx-translate-multi-http-loader';
-import {VersionModule} from '../version/version.module';
-import {FormsModule} from '@angular/forms';
-import {DmpActionsModule} from './dmp-actions/dmp-actions.module';
+import {ReuseModule} from './reuse/reuse.module';
 import {RouterModule} from '@angular/router';
-import {DMP_ROUTES} from './dmp.routes';
+import {SpecifyDataModule} from './specify-data/specify-data.module';
+import {SummaryModule} from './summary/summary.module';
+import {VersionModule} from '../version/version.module';
 
 // required for AOT compilation
 export function HttpLoaderFactory(http: HttpClient): MultiTranslateHttpLoader {
