bca3f5af9540bdf7c99d216e95232c86768b6e03
Clara Schuster <98585998+clarascu@users.noreply.github.com>
2022-12-15 16:02:55 +0100

    Fix tests

--- libs/damap/src/lib/components/access/access.component.spec.ts
+++ libs/damap/src/lib/components/access/access.component.spec.ts
@@ -1,14 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { AccessComponent } from './access.component';
+import { AccessComponent } from "./access.component";
+import { InfoMessageModule } from "../../widgets/info-message/info-message.module";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatIconModule } from "@angular/material/icon";
+import { PersonCardComponent } from "../../widgets/person-card/person-card.component";
+import { BackendService, TranslateTestingModule } from "@damap/core";
+import { ActivatedRoute } from "@angular/router";
+import { mockAccess } from "../../mocks/access-mocks";
+import { completeDmp } from "../../mocks/dmp-mocks";
+import { of } from "rxjs";
+import { RouterTestingModule } from "@angular/router/testing";
 
-describe('AccessComponent', () => {
+describe("AccessComponent", () => {
   let component: AccessComponent;
   let fixture: ComponentFixture<AccessComponent>;
+  let backendSpy;
 
   beforeEach(async () => {
+    backendSpy = jasmine.createSpyObj("BackendService",
+      ["getDmpById", "getAccess", "createAccess", "deleteAccess"]);
+    backendSpy.getDmpById.and.returnValue(of([completeDmp]));
+    backendSpy.getAccess.and.returnValue(of([mockAccess]));
     await TestBed.configureTestingModule({
-      imports: [AccessComponent],
+      imports: [
+        AccessComponent, TranslateTestingModule,
+        PersonCardComponent, InfoMessageModule,
+        RouterTestingModule.withRoutes([]),
+        MatButtonModule, MatCheckboxModule, MatIconModule],
+      providers: [
+        { provide: BackendService, useValue: backendSpy },
+        { provide: ActivatedRoute, useValue: { snapshot: { paramMap: { get: (id: number) => mockAccess.dmpId } } } }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AccessComponent);
@@ -16,7 +40,7 @@ describe('AccessComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
