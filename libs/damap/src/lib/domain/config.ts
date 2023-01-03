e65c77cf5b057a500aef80bf7d154bcf1a88f604
Laura TH <112856634+lpandath@users.noreply.github.com>
2023-01-03 13:18:06 +0100

    15 show search field for search services in drop down menu (#23)

--- libs/damap/src/lib/domain/config.ts
+++ libs/damap/src/lib/domain/config.ts
@@ -1,7 +1,10 @@
+import { ServiceConfig } from "./config-services";
+
 export interface Config {
   readonly authUrl: string;
   readonly authClient: string;
   readonly authScope: string;
   readonly authUser: string;
   readonly env: string;
+  readonly personSearchServiceConfigs: ServiceConfig[];
 }
