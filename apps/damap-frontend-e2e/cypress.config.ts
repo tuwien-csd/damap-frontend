0171631f3fbe7069dc8105070b5720f836b4c8cb
Clara Schuster <98585998+clarascu@users.noreply.github.com>
2023-01-04 15:55:57 +0100

    Add delete dmp e2e test (#81)

--- apps/damap-frontend-e2e/cypress.config.ts
+++ apps/damap-frontend-e2e/cypress.config.ts
@@ -5,8 +5,10 @@ export default defineConfig({
   e2e: {
     ...nxE2EPreset(__dirname),
     specPattern: [
+      "**/app.cy.ts",
       "**/dmp.cy.ts",
       "**/access.cy.ts",
+      "**/delete-dmp.cy.ts",
       "**/*.cy.ts"
     ],
   },
