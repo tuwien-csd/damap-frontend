2bf8cf8965f20fcc2936a9d36e93c34c0c317efe
David Eckhard <dabombeeer@gmail.com>
2023-02-14 13:17:14 +0100

    create reusable method for comparing contributors

--- libs/damap/src/lib/components/dmp/people/contributor-filter.pipe.ts
+++ libs/damap/src/lib/components/dmp/people/contributor-filter.pipe.ts
@@ -1,24 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import { Contributor } from '../../../domain/contributor';
+import { compareContributors, Contributor } from '../../../domain/contributor';
 
 @Pipe({
-  name: 'contributorFilter'
+  name: 'contributorFilter',
 })
 export class ContributorFilterPipe implements PipeTransform {
-
-  transform(projectMembers: Contributor[], contributors: Contributor[]): Contributor[] {
+  transform(
+    projectMembers: Contributor[],
+    contributors: Contributor[]
+  ): Contributor[] {
     return projectMembers?.filter(
       entry =>
-        // filter contributors that are already in the list based on universityId and personId
+        // filter contributors that are already in the list
         contributors.find(c => {
-          const universityId = c.universityId && entry.universityId && c.universityId === entry.universityId;
-          const personId = c.personId && entry.personId &&
-            c.personId.type === entry.personId.type &&
-            c.personId.identifier === entry.personId.identifier;
-          return (universityId || personId);
+          return compareContributors(c, entry);
         }) === undefined
     );
   }
-
 }
--- libs/damap/src/lib/domain/contributor.ts
+++ libs/damap/src/lib/domain/contributor.ts
@@ -1,7 +1,7 @@
-import {ContributorRole} from './enum/contributor-role.enum';
-import {Identifier} from './identifier';
+import { ContributorRole } from './enum/contributor-role.enum';
+import { Identifier } from './identifier';
 
-export interface Contributor {
+interface Contributor {
   id: number;
   readonly universityId: string;
   readonly personId: Identifier;
@@ -14,3 +14,16 @@ export interface Contributor {
   role: ContributorRole;
   roleInProject: string;
 }
+
+function compareContributors(c1: Contributor, c2: Contributor) {
+  const universityId =
+    c1.universityId && c2.universityId && c1.universityId === c2.universityId;
+  const personId =
+    c1.personId &&
+    c2.personId &&
+    c1.personId.type === c2.personId.type &&
+    c1.personId.identifier === c2.personId.identifier;
+  return universityId || personId;
+}
+
+export { compareContributors, Contributor };
