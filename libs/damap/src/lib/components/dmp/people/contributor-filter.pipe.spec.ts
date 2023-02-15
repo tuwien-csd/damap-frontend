b7da98fc3b3174b92c20afb26e9b94e324799c9e
David Eckhard <dabombeeer@gmail.com>
2023-02-15 16:53:37 +0100

    add testcases

--- libs/damap/src/lib/components/dmp/people/contributor-filter.pipe.spec.ts
+++ libs/damap/src/lib/components/dmp/people/contributor-filter.pipe.spec.ts
@@ -1,3 +1,4 @@
+import { compareContributors, Contributor } from '../../../domain/contributor';
 import {
   mockContributor1,
   mockContributor2,
@@ -10,14 +11,29 @@ describe('ContributorFilterPipe', () => {
     expect(pipe).toBeTruthy();
   });
 
-  it('should filter Contributors', () => {
+  it('should filter already added contributors', () => {
     const pipe = new ContributorFilterPipe();
     const addedContributors = [mockContributor1];
     const contributorSearchResult = [mockContributor1, mockContributor2];
 
-    var filteredContributors = pipe.transform(contributorSearchResult, addedContributors);
+    const filteredContributors = pipe.transform(
+      contributorSearchResult,
+      addedContributors
+    );
     expect(filteredContributors);
     expect(filteredContributors.length).toBe(1);
     expect(filteredContributors[0]).toBe(mockContributor2);
+  });
+
+  it('should compare contributors', () => {
+    let c2 = mockContributor2;
+    expect(compareContributors(c2, c2)).toBeTruthy();
+    expect(compareContributors(null, c2)).toBeFalsy();
+    expect(compareContributors(c2, null)).toBeFalsy();
+
+    let c2NoUniversityID: Contributor = { ...c2, universityId: null };
+    expect(
+      compareContributors(c2NoUniversityID, c2NoUniversityID)
+    ).toBeTruthy();
   });
 });
--- libs/damap/src/lib/services/form.service.spec.ts
+++ libs/damap/src/lib/services/form.service.spec.ts
@@ -1,15 +1,17 @@
-import {TestBed} from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-import {FormService} from './form.service';
-import {UntypedFormBuilder} from '@angular/forms';
-import {completeDmp, noDataDmp} from '../mocks/dmp-mocks';
+import { UntypedFormArray, UntypedFormBuilder } from '@angular/forms';
+import { Contributor } from '../domain/contributor';
+import { mockContributor1, mockContributor2 } from '../mocks/contributor-mocks';
+import { completeDmp, noDataDmp } from '../mocks/dmp-mocks';
+import { FormService } from './form.service';
 
 describe('FormService', () => {
   let service: FormService;
   let formbuilder: UntypedFormBuilder;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({providers: [UntypedFormBuilder]});
+    TestBed.configureTestingModule({ providers: [UntypedFormBuilder] });
     service = TestBed.inject(FormService);
     formbuilder = TestBed.inject(UntypedFormBuilder);
   });
@@ -24,5 +26,44 @@ describe('FormService', () => {
     expect(service.exportFormToDmp()).toEqual(completeDmp);
     service.mapDmpToForm(noDataDmp);
     expect(service.exportFormToDmp()).toEqual(noDataDmp);
-  })
+  });
+
+  it('should add/remove/mark contributors', () => {
+    let contributorFormArray = getContributorFormArray();
+
+    // add contributor
+    service.addContributorToForm(mockContributor1);
+    expect(getContributorFromForm(0)).toEqual(mockContributor1);
+    expect(contributorFormArray.length).toBe(1);
+
+    // remove contributor
+    service.removeContributorFromForm(0);
+    expect(contributorFormArray.length).toBe(0);
+
+    // add as contact
+    service.addContributorToForm(mockContributor1);
+    expect(getContributorFromForm(0)).toEqual(mockContributor1);
+    service.addContributorToForm(mockContributor2, true);
+    expect(contributorFormArray.length).toEqual(2);
+    expect(getContributorFromForm(1)).toEqual({
+      ...mockContributor2,
+      contact: true,
+    });
+
+    // change contact
+    service.changeContactPerson(mockContributor1);
+    expect(getContributorFromForm(0)).toEqual({
+      ...mockContributor1,
+      contact: true,
+    });
+    expect(getContributorFromForm(1)).toEqual(mockContributor2);
+  });
+
+  function getContributorFromForm(index: number): Contributor {
+    return getContributorFormArray().at(index).getRawValue();
+  }
+
+  function getContributorFormArray() {
+    return service.dmpForm.get('contributors') as UntypedFormArray;
+  }
 });
