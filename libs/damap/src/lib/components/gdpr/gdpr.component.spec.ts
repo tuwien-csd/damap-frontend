1eec7cb38e4fce9dd641a44365575568a5a79935
Clara Schuster <98585998+clarascu@users.noreply.github.com>
2023-02-21 10:50:01 +0100

    Fix GDPR test

--- libs/damap/src/lib/components/gdpr/gdpr.component.spec.ts
+++ libs/damap/src/lib/components/gdpr/gdpr.component.spec.ts
@@ -1,14 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GdprComponent } from './gdpr.component';
+import { BackendService, TranslateTestingModule } from "@damap/core";
 
 describe('GdprComponent', () => {
   let component: GdprComponent;
   let fixture: ComponentFixture<GdprComponent>;
+  let backendSpy;
 
   beforeEach(async () => {
+    backendSpy = jasmine.createSpyObj('BackendService', ['getGdpr']);
     await TestBed.configureTestingModule({
-      imports: [GdprComponent],
+      imports: [GdprComponent, TranslateTestingModule],
+      providers: [{provide: BackendService, useValue: backendSpy}]
     }).compileComponents();
 
     fixture = TestBed.createComponent(GdprComponent);
