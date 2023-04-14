f2257c2876eb3bf4b8317fb815efb7b7636c3dd7
Zeno Casellato <zeno.casellato@tuwien.ac.at>
2023-04-14 16:20:51 +0200

    capitalize license id to coinceide with backend enums,
    refs GH-https://github.com/tuwien-csd/damap-backend/issues/91

--- libs/damap/src/lib/widgets/license-wizard/license-wizard-list.ts
+++ libs/damap/src/lib/widgets/license-wizard/license-wizard-list.ts
@@ -2,7 +2,7 @@ import {LicenseDetails} from '../../domain/license-details';
 
 // Licenses
 export const ccPublicDomain: LicenseDetails = {
-  id: 'ccPublicDomain',
+  id: 'CCPUBLICDOMAIN',
   name: 'license.wizard.license.ccPublicDomain.name',
   priority: 1,
   available: true,
@@ -14,7 +14,7 @@ export const ccPublicDomain: LicenseDetails = {
   compatibility: ['cc-public-domain', 'mit', 'bsd-2c', 'bsd-3c', 'apache-2', 'lgpl-2.1', 'lgpl-2.1+', 'lgpl-3', 'mpl-2', 'epl-1', 'cddl-1', 'gpl-2', 'gpl-2+', 'gpl-3', 'agpl-1', 'agpl-3']
 };
 export const ccZero = {
-  id: 'ccZero',
+  id: 'CCZERO',
   name: 'license.wizard.license.ccZero.name',
   priority: 1,
   available: true,
@@ -24,7 +24,7 @@ export const ccZero = {
   labels: ['public', 'cc', 'zero', 'opendata'],
 };
 export const pddl = {
-  id: 'pddl',
+  id: 'PDDL',
   name: 'license.wizard.license.pddl.name',
   priority: 1,
   available: false,
@@ -34,7 +34,7 @@ export const pddl = {
   labels: ['public']
 };
 export const ccBy = {
-  id: 'ccBy',
+  id: 'CCBY',
   name: 'license.wizard.license.ccBy.name',
   priority: 1,
   available: true,
@@ -44,7 +44,7 @@ export const ccBy = {
   labels: ['public', 'cc', 'by', 'opendata']
 };
 export const odcBy = {
-  id: 'odcBy',
+  id: 'ODCBY',
   name: 'license.wizard.license.odcBy.name',
   priority: 1,
   available: false,
@@ -54,7 +54,7 @@ export const odcBy = {
   labels: ['public']
 };
 export const ccBySa = {
-  id: 'ccBySa',
+  id: 'CCBYSA',
   name: 'license.wizard.license.ccBySa.name',
   priority: 1,
   available: true,
@@ -64,7 +64,7 @@ export const ccBySa = {
   labels: ['public', 'cc', 'by', 'sa', 'opendata']
 };
 export const odbl = {
-  id: 'odbl',
+  id: 'ODBL',
   name: 'license.wizard.license.odbl.name',
   priority: 1,
   available: false,
@@ -74,7 +74,7 @@ export const odbl = {
   labels: ['public']
 };
 export const ccByNd = {
-  id: 'ccByNd',
+  id: 'CCBYND',
   name: 'license.wizard.license.ccByNd.name',
   priority: 1,
   available: true,
@@ -84,7 +84,7 @@ export const ccByNd = {
   labels: ['public', 'cc', 'nd']
 };
 export const ccByNc = {
-  id: 'ccByNc',
+  id: 'CCBYNC',
   name: 'license.wizard.license.ccByNc.name',
   priority: 1,
   available: true,
@@ -94,7 +94,7 @@ export const ccByNc = {
   labels: ['public', 'cc', 'nc']
 };
 export const ccByNcSa = {
-  id: 'ccByNcSa',
+  id: 'CCBYNCSA',
   name: 'license.wizard.license.ccByNcSa.name',
   priority: 1,
   available: true,
@@ -104,7 +104,7 @@ export const ccByNcSa = {
   labels: ['public', 'cc', 'by', 'nc', 'sa']
 };
 export const ccByNcNd = {
-  id: 'ccByNcNd',
+  id: 'CCBYNCND',
   name: 'license.wizard.license.ccByNcNd.name',
   priority: 1,
   available: true,
@@ -114,7 +114,7 @@ export const ccByNcNd = {
   labels: ['public', 'cc', 'by', 'nc', 'nd']
 };
 export const perlArtistic1 = {
-  id: 'perlArtistic1',
+  id: 'PERLARTISTIC1',
   name: 'license.wizard.license.perlArtistic1.name',
   priority: 7,
   available: true,
@@ -124,7 +124,7 @@ export const perlArtistic1 = {
   labels: ['public', 'perl']
 };
 export const perlArtistic2 = {
-  id: 'perlArtistic2',
+  id: 'PERLARTISTIC2',
   name: 'license.wizard.license.perlArtistic2.name',
   priority: 8,
   available: true,
@@ -134,7 +134,7 @@ export const perlArtistic2 = {
   labels: ['public', 'perl', 'osi']
 };
 export const gpl2plus = {
-  id: 'gpl2plus',
+  id: 'GPL2PLUS',
   name: 'license.wizard.license.gpl2plus.name',
   priority: 10,
   available: true,
@@ -146,7 +146,7 @@ export const gpl2plus = {
   compatibility: ['gpl-2', 'gpl-2+', 'gpl-3', 'agpl-1', 'agpl-3']
 };
 export const gpl2 = {
-  id: 'gpl2',
+  id: 'GPL2',
   name: 'license.wizard.license.gpl2.name',
   priority: 10,
   available: false,
@@ -158,7 +158,7 @@ export const gpl2 = {
   compatibility: ['gpl-2', 'agpl-1']
 };
 export const gpl3 = {
-  id: 'gpl3',
+  id: 'GPL3',
   name: 'license.wizard.license.gpl3.name',
   priority: 11,
   available: true,
@@ -170,7 +170,7 @@ export const gpl3 = {
   compatibility: ['gpl-2+', 'gpl-3', 'agpl-3']
 };
 export const agpl1 = {
-  id: 'agpl1',
+  id: 'AGPL1',
   name: 'license.wizard.license.agpl1.name',
   priority: 50,
   available: false,
@@ -182,7 +182,7 @@ export const agpl1 = {
   compatibility: ['agpl-1']
 };
 export const agpl3 = {
-  id: 'agpl3',
+  id: 'AGPL3',
   name: 'license.wizard.license.agpl3.name',
   priority: 51,
   available: true,
@@ -194,7 +194,7 @@ export const agpl3 = {
   compatibility: ['agpl-3']
 };
 export const mpl2 = {
-  id: 'mpl2',
+  id: 'MPL2',
   name: 'license.wizard.license.mpl2.name',
   priority: 1,
   available: true,
@@ -206,7 +206,7 @@ export const mpl2 = {
   compatibility: ['lgpl-2.1', 'lgpl-2.1+', 'lgpl-3', 'mpl-2', 'gpl-2', 'gpl-2+', 'gpl-3', 'agpl-1', 'agpl-3']
 };
 export const lgpl21plus = {
-  id: 'lgpl21plus',
+  id: 'LGPL21PLUS',
   name: 'license.wizard.license.lgpl21plus.name',
   priority: 2,
   available: true,
@@ -218,7 +218,7 @@ export const lgpl21plus = {
   compatibility: ['lgpl-2.1+', 'lgpl-3', 'mpl-2', 'gpl-2', 'gpl-2+', 'gpl-3', 'agpl-1', 'agpl-3']
 };
 export const lgpl21 = {
-  id: 'lgpl21',
+  id: 'LGPL21',
   name: 'license.wizard.license.lgpl21.name',
   priority: 2,
   available: false,
@@ -230,7 +230,7 @@ export const lgpl21 = {
   compatibility: ['lgpl-2.1', 'mpl-2', 'gpl-2', 'agpl-1']
 };
 export const lgpl3 = {
-  id: 'lgpl3',
+  id: 'LGPL3',
   name: 'license.wizard.license.lgpl3.name',
   priority: 3,
   available: true,
@@ -242,7 +242,7 @@ export const lgpl3 = {
   compatibility: ['lgpl-3', 'mpl-2', 'gpl-3', 'agpl-3']
 };
 export const epl1 = {
-  id: 'epl1',
+  id: 'EPL1',
   name: 'license.wizard.license.epl1.name',
   priority: 4,
   available: true,
@@ -254,7 +254,7 @@ export const epl1 = {
   compatibility: ['mpl-2', 'epl-1', 'cddl-1', 'gpl-3', 'agpl-3']
 };
 export const cddl1 = {
-  id: 'cddl1',
+  id: 'CDDL1',
   name: 'license.wizard.license.cddl1.name',
   priority: 5,
   available: true,
@@ -266,7 +266,7 @@ export const cddl1 = {
   compatibility: ['cddl-1']
 };
 export const mit = {
-  id: 'mit',
+  id: 'MIT',
   name: 'license.wizard.license.mit.name',
   priority: 1,
   available: true,
@@ -278,7 +278,7 @@ export const mit = {
   compatibility: ['mit', 'bsd-2c', 'bsd-3c', 'apache-2', 'lgpl-2.1', 'lgpl-2.1+', 'lgpl-3', 'mpl-2', 'epl-1', 'cddl-1', 'gpl-2', 'gpl-2+', 'gpl-3', 'agpl-1', 'agpl-3']
 };
 export const bsd3c = {
-  id: 'bsd3c',
+  id: 'BSD3C',
   name: 'license.wizard.license.bsd3c.name',
   priority: 2,
   available: true,
@@ -290,7 +290,7 @@ export const bsd3c = {
   compatibility: ['bsd-3c', 'apache-2', 'lgpl-2.1', 'lgpl-2.1+', 'lgpl-3', 'mpl-2', 'epl-1', 'cddl-1', 'gpl-2', 'gpl-2+', 'gpl-3', 'agpl-1', 'agpl-3']
 };
 export const bsd2c = {
-  id: 'bsd2c',
+  id: 'BSD2C',
   name: 'license.wizard.license.bsd2c.name',
   priority: 3,
   available: true,
@@ -302,7 +302,7 @@ export const bsd2c = {
   compatibility: ['bsd-2c', 'bsd-3c', 'apache-2', 'lgpl-2.1', 'lgpl-2.1+', 'lgpl-3', 'mpl-2', 'epl-1', 'cddl-1', 'gpl-2', 'gpl-2+', 'gpl-3', 'agpl-1', 'agpl-3']
 };
 export const apache2 = {
-  id: 'apache2',
+  id: 'APACHE2',
   name: 'license.wizard.license.apache2.name',
   priority: 4,
   available: true,
