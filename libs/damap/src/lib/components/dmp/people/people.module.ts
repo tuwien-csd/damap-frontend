069d5bacb8c8722b94ad9e8ed88ff5ba96cc3cf6
lpandath <112856634+lpandath@users.noreply.github.com>
2024-04-11 15:46:23 +0200

    format prettier into all files

--- libs/damap/src/lib/components/dmp/people/people.module.ts
+++ libs/damap/src/lib/components/dmp/people/people.module.ts
@@ -3,17 +3,17 @@ import {
   PeopleComponent,
 } from './people.component';
 
-import {ContributorFilterPipe} from './contributor-filter.pipe';
-import {ContributorManualComponent} from './contributor-manual/contributor-manual.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatSelectModule} from '@angular/material/select';
-import {NgModule} from '@angular/core';
-import {OrcidModule} from '../../../widgets/orcid/orcid.module';
-import {PersonSearchModule} from '../../../widgets/person-search/person-search.module';
-import {SharedModule} from '../../../shared/shared.module';
-import {StepIntroModule} from '../../../widgets/step-intro/step-intro.module';
+import { ContributorFilterPipe } from './contributor-filter.pipe';
+import { ContributorManualComponent } from './contributor-manual/contributor-manual.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { NgModule } from '@angular/core';
+import { OrcidModule } from '../../../widgets/orcid/orcid.module';
+import { PersonSearchModule } from '../../../widgets/person-search/person-search.module';
+import { SharedModule } from '../../../shared/shared.module';
+import { StepIntroModule } from '../../../widgets/step-intro/step-intro.module';
 import { TooltipModule } from '../../../widgets/tooltip/tooltip.module';
 
 @NgModule({
@@ -28,7 +28,7 @@ import { TooltipModule } from '../../../widgets/tooltip/tooltip.module';
     MatSelectModule,
     MatButtonModule,
     MatDialogModule,
-    TooltipModule
+    TooltipModule,
   ],
   declarations: [
     PeopleComponent,
@@ -48,7 +48,7 @@ import { TooltipModule } from '../../../widgets/tooltip/tooltip.module';
     MatSelectModule,
     MatButtonModule,
     MatDialogModule,
-    TooltipModule
-  ]
+    TooltipModule,
+  ],
 })
 export class PeopleModule {}
