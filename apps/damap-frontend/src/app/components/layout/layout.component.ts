7d09488fdebdc6d8ea663ff7d6f5fa6b506eb35d
Laura TH <lthaci@tugraz.at>
2024-07-25 14:47:08 +0200

    Implement navbar toggle and responsive layout adjustments

--- apps/damap-frontend/src/app/components/layout/layout.component.ts
+++ apps/damap-frontend/src/app/components/layout/layout.component.ts
@@ -1,20 +1,31 @@
-import pkg from '../../../../../../package.json'; // eslint-disable-line
-import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+
 import { AuthService } from '@damap/core';
 import { ConfigService } from '../../services/config.service';
+import { MatSidenav } from '@angular/material/sidenav';
+import { TranslateService } from '@ngx-translate/core';
+import pkg from '../../../../../../package.json'; // eslint-disable-line
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css'],
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit, AfterViewInit {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
   public title = 'Data Management Plan';
   public version: string = pkg.version;
   public name: string;
   public lang = 'en';
-  public widescreen = () => window.innerWidth >= 1024;
+  public isSmallScreen: boolean = false;
 
   readonly env: string;
 
@@ -22,6 +33,7 @@ export class LayoutComponent implements OnInit {
     private auth: AuthService,
     private translate: TranslateService,
     private configService: ConfigService,
+    private cdr: ChangeDetectorRef,
   ) {
     this.env = this.configService.getEnvironment();
   }
@@ -33,6 +45,25 @@ export class LayoutComponent implements OnInit {
     this.lang = this.translate.currentLang.toUpperCase();
   }
 
+  ngAfterViewInit(): void {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', [])
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isSmallScreen = window.innerWidth < 1024;
+    if (this.isSmallScreen) {
+      this.sidenav.close();
+    } else {
+      this.sidenav.open();
+    }
+    this.cdr.detectChanges();
+  }
+
   useLanguage(language: string): void {
     this.lang = language.toUpperCase();
     this.translate.use(language);
@@ -40,5 +71,9 @@ export class LayoutComponent implements OnInit {
 
   public logout() {
     this.auth.logout();
+  }
+
+  toggleSidenav() {
+    this.sidenav.toggle();
   }
 }
