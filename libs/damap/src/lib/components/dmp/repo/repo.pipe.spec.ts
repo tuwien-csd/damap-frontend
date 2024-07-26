8fe3929c9dc7b5d4a2d5e1b0ca82f5c2deb17e1e
David Eckhard <dabombeeer@gmail.com>
2024-07-26 10:40:33 +0200

    repos: pass filtered recommended repositories

--- libs/damap/src/lib/components/dmp/repo/repo.pipe.spec.ts
+++ libs/damap/src/lib/components/dmp/repo/repo.pipe.spec.ts
@@ -1,8 +1,44 @@
+import { Repository } from '../../../domain/repository';
+import { RepositoryDetails } from '../../../domain/repository-details';
 import { RepoPipe } from './repo.pipe';
 
 describe('RepoPipe', () => {
   it('create an instance', () => {
     const pipe = new RepoPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should filter already selected repositories', () => {
+    const pipe = new RepoPipe();
+
+    let repos: RepositoryDetails[] = [
+      {
+        id: '1',
+        name: 'one',
+      },
+      {
+        id: '2',
+        name: 'two',
+      },
+    ];
+
+    let selectedRepos: Repository[] = [
+      {
+        repositoryId: '1',
+        id: 1,
+        title: 'one',
+      },
+      {
+        repositoryId: '3',
+        id: 3,
+        title: 'three ',
+      },
+    ];
+    let filtered = pipe.transform(repos, selectedRepos);
+    expect(filtered.length).toBe(1);
+    expect(filtered.at(0)).toEqual({
+      id: '2',
+      name: 'two',
+    });
   });
 });
